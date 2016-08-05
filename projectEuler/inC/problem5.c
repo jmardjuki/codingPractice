e1ac0f2d073696924119ea6b04dc260d4b40b87e
Janet Mardjuki <mardjuki@sfu.ca>
2016-08-04 18:26:32 -0700

    Initial commit

---
+++ projectEuler/inC/problem5.c
@@ -0,0 +1,17 @@
+/*
+ * 
+ * Notes: Evenly Divisible means it can be divided without remainder
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MIN_NUM 1
+#define MAX_NUM 20
+
+
+int main(void)
+{
+
+    return 0;
+}
