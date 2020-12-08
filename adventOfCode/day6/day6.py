0782f547a09f14b9ce45c12acff32574a4655c62
Janet Mardjuki <mardjuki@sfu.ca>
2020-12-07 17:57:36 -0800

    Part2

--- adventOfCode/day6/day6.py
+++ adventOfCode/day6/day6.py
@@ -8,16 +8,35 @@ def countForm(group:list)->int:
                 ticks.add(char)
                 count+=1
     return count
-        
+
+def countForm2(group:list)->int:
+    count = 0
+    ticks = {}
+    entries = group.split("\n")
+    numOfPeople = len(entries)
+    for entry in entries:
+        if numOfPeople == 1:
+            return len(entry)
+        for char in entry:
+            if char not in ticks:
+                ticks[char] = 1
+            else:
+                ticks[char]+=1
+                if ticks[char] == numOfPeople:
+                    count+=1
+    return count
 
 def main():
     f = open("input.txt", 'r')
     lines = f.read()
     groups = lines.split("\n\n")
     result = 0
+    result2 = 0
     for group in groups:
         result += countForm(group)
+        result2 += countForm2(group)
     print(result)
+    print(result2)
 
 if __name__ == "__main__":
     main()
