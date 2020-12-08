27531450d2ca8d43f5eecb564ff2fbe5bd699499
Janet Mardjuki <mardjuki@sfu.ca>
2020-12-08 11:23:31 -0800

    Added day4 part2

--- adventOfCode/day4/day4.py
+++ adventOfCode/day4/day4.py
@@ -1,14 +1,60 @@
+import re
+
+def checkItemValidity(val:list)->bool:
+    if val[0] == 'byr':
+        byr = int(val[1])
+        if byr <= 2002 and byr >= 1920:
+            return True
+    if val[0] == 'iyr':
+        iyr = int(val[1])
+        if iyr <= 2020 and iyr >= 2010:
+            return True
+    if val[0] == 'eyr':
+        eyr = int(val[1])
+        if eyr <= 2030 and eyr >= 2020:
+            return True
+    if val[0] == 'hgt':
+        hgt = val[1]
+        p = re.compile("(.[0-9]*)(.[a-z]*)")
+        result = p.search(hgt)
+        height = int(result.group(1))
+        unit = result.group(2)
+        if (unit == "cm"):
+            if(height >= 150 and height <= 193):
+                return True
+        elif (unit == "in"):
+            if(height >= 59 and height <= 76):
+                return True
+    if val[0] == 'hcl':
+        p = re.compile("#[0-9a-f]{6}")
+        result = p.search(val[1])
+        if result:
+            return True
+    if val[0] == 'ecl':
+        colourValid = {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}
+        if val[1] in colourValid:
+            return True
+    if val[0] == 'pid':
+        p = re.compile("[0-9]{9}")
+        result = p.search(val[1])
+        if result and len(val[1]) == 9:
+            return True
+
+    return False
+        
+
 def checkValidity(detail:list)->bool:
     mandatory = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
     for val in detail:
         val = val.split(":")
         if val[0] in mandatory:
-            mandatory.remove(val[0])
+            if(checkItemValidity(val)):
+                mandatory.remove(val[0])
     if len(mandatory) > 0:
         return False
     return True
 
-def part1(details:list)->int:
+def part(details:list)->int:
     count = 0
     for detail in details:
         detail = detail.split()
@@ -22,7 +68,7 @@ def main():
     lines = f.read()
     lines = lines.split("\n\n")
         
-    print(part1(lines))    
+    print(part(lines))    
     
 if __name__ == "__main__":
     main()
